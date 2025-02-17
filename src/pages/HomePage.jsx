5f60e8028aad494faba32f669ae18c190623d2d3
[200~Richieconceptgit config --global user.name Richieconceptgit config --global user.name [200~Richieconcept <richieconcept01@gmail.com>
2025-02-17 18:25:08 +0100

    deployment issues

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../component/navbar/navbar';
+import navbar from "../component/navbar/Navbar"
 import { Container, Row, Col } from "react-bootstrap";
 
 function HomePage() {
